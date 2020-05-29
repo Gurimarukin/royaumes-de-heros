03ddbe01e8962c52af74ff2cfa9c52941e628f5a
Gurimarukin <naxel@laposte.net>
2020-05-29 15:08:02 +0200

    Create WildTest

---
+++ apps/heros/test/cards/wild_test.exs
@@ -0,0 +1,45 @@
+defmodule Heros.Cards.WildTest do
+  use ExUnit.Case, async: true
+
+  alias Heros.{Cards, Game, Player}
+  alias Heros.Cards.Card
+
+  test "broelyn" do
+  end
+
+  test "cron" do
+  end
+
+  test "dire_wolf" do
+  end
+
+  test "elven_curse" do
+  end
+
+  test "elven_gift" do
+  end
+
+  test "grak" do
+  end
+
+  test "natures_bounty" do
+  end
+
+  test "orc_grunt" do
+  end
+
+  test "rampage" do
+  end
+
+  test "torgen" do
+  end
+
+  test "spark" do
+  end
+
+  test "wolf_form" do
+  end
+
+  test "wolf_shaman" do
+  end
+end
