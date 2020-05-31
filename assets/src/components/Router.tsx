88ef57e31b75f186b44e1a2bc30a0176b5e6366d
Gurimarukin <naxel@laposte.net>
2020-05-31 17:01:32 +0200

    Add route for game

--- assets/src/components/Router.tsx
+++ assets/src/components/Router.tsx
@@ -1,10 +1,15 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
-import { FunctionComponent, useEffect, ReactElement } from 'react'
+import { parser as P, string as S, char as C } from 'parser-ts'
+import { run } from 'parser-ts/lib/code-frame'
+import { useEffect, ReactElement } from 'react'
 
 import { NotFound } from './NotFound'
 import { Squads } from './Squads'
-import { Maybe, pipe, Dict } from '../utils/fp'
+import { Maybe, pipe, Dict, List } from '../utils/fp'
+
+type Route = (path: string) => Maybe<SubtitleWithElt>
+type SubtitleWithElt = [Maybe<string>, ReactElement]
 
 interface Props {
   readonly path: string
@@ -30,11 +35,51 @@ export namespace Router {
 
 const routes = Router.routes
 
-function route(path: string): [Maybe<string>, ReactElement] {
+function route(path: string): SubtitleWithElt {
   return pipe(
-    Dict.lookup<[Maybe<string>, ReactElement]>(path, {
-      [routes.squads]: [Maybe.none, <Squads />]
-    }),
+    [
+      exactMatcher(routes.squads)(() => [Maybe.none, <Squads />]),
+      simpleExtractor('game')(id => [Maybe.none, <div>Game {id}</div>])
+    ],
+    List.reduce<Route, Maybe<SubtitleWithElt>>(Maybe.none, (acc, route) =>
+      pipe(
+        acc,
+        Maybe.alt(() => route(path))
+      )
+    ),
     Maybe.getOrElse(() => [Maybe.some('Page non trouvée'), <NotFound />])
   )
 }
+
+function exactMatcher(str: string): (f: () => SubtitleWithElt) => Route {
+  return f => path =>
+    pipe(
+      str === path ? Maybe.some(path) : Maybe.none,
+      Maybe.map(_ => f())
+    )
+}
+
+// matches "/prefix/:param"
+function simpleExtractor(prefix: string): (f: (a: string) => SubtitleWithElt) => Route
+function simpleExtractor<A>(
+  prefix: string,
+  argParser: P.Parser<string, A>
+): (f: (a: A) => SubtitleWithElt) => Route
+function simpleExtractor<A>(
+  prefix: string,
+  argParser: P.Parser<string, A> = (alphanum as unknown) as P.Parser<string, A>
+): (f: (a: A) => SubtitleWithElt) => Route {
+  const parser = pipe(
+    S.string(`/${prefix}/`),
+    P.chain(_ => argParser),
+    P.chain(res =>
+      pipe(
+        P.eof<string>(),
+        P.map(_ => res)
+      )
+    )
+  )
+  return f => path => pipe(run(parser, path), Maybe.fromEither, Maybe.map(f))
+}
+
+const alphanum = S.many(C.alphanum)
