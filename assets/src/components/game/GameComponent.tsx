ef83401a694bf04a461b37ecce5b6ef3966bf501
Gurimarukin <naxel@laposte.net>
2020-06-02 12:02:37 +0200

    Refacto

--- assets/src/components/game/GameComponent.tsx
+++ assets/src/components/game/GameComponent.tsx
@@ -10,7 +10,7 @@ interface Props {
   readonly state: Game
 }
 
-interface Pos {
+interface BoardProps {
   readonly s: number
   readonly x: number
   readonly y: number
@@ -19,7 +19,7 @@ interface Pos {
 const maxScale = 1.5
 
 export const GameComponent: FunctionComponent<Props> = ({ call, state }) => {
-  const posRef = useRef<Pos>({ s: 1, x: 0, y: window.innerHeight - board.height })
+  const posRef = useRef<BoardProps>({ s: 1, x: 0, y: window.innerHeight - board.height })
   const [props, set] = useSpring(() => ({ s: 1, x: 0, y: window.innerHeight - board.height }))
 
   // ({ deltaY }: React.WheelEvent) => setScale(scale.current + deltaY / (-50 * scale.current)),
@@ -28,11 +28,7 @@ export const GameComponent: FunctionComponent<Props> = ({ call, state }) => {
     <div css={styles.container} onWheel={onWheel}>
       <a.div
         css={styles.board}
-        style={{
-          transform: props.s.interpolate(trans),
-          left: props.x,
-          top: props.y
-        }}
+        style={{ transform: props.s.interpolate(trans), left: props.x, top: props.y }}
       />
       <pre css={stylesPre}>{JSON.stringify(state, null, 2)}</pre>
       {/* <button onClick={play}>Jouer</button> */}
@@ -40,43 +36,63 @@ export const GameComponent: FunctionComponent<Props> = ({ call, state }) => {
   )
 
   function onWheel({ deltaY, clientX, clientY }: React.WheelEvent) {
-    const zoomIn = deltaY < 0
+    // const zoomIn = deltaY < 0
     const minScaleW = window.innerWidth / board.width
     const minScaleH = window.innerHeight / board.height
 
-    const s = getS()
-    const getX = getCoord(window.innerWidth, board.width, posRef.current.x, clientX)
-    const getY = getCoord(window.innerHeight, board.height, posRef.current.y, clientY)
+    const s = getS(posRef.current.s, deltaY, minScaleW, minScaleH)
 
-    posRef.current = {
+    setBoardProps({
       s,
-      x: s < minScaleW ? (window.innerWidth - board.width * s) / 2 : getX(s),
-      y: s < minScaleH ? (window.innerHeight - board.height * s) / 2 : getY(s)
-    }
-    set(posRef.current)
+      x: getCoord(
+        posRef.current.s,
+        minScaleW,
+        window.innerWidth,
+        board.width,
+        posRef.current.x,
+        clientX,
+        s
+      ),
+      y: getCoord(
+        posRef.current.s,
+        minScaleH,
+        window.innerHeight,
+        board.height,
+        posRef.current.y,
+        clientY,
+        s
+      )
+    })
 
-    function getS() {
-      const minScale = Math.min(minScaleW, minScaleH)
-      const scale = posRef.current.s + deltaY * -0.03
-      return scale < minScale ? minScale : maxScale < scale ? maxScale : scale
-    }
-
-    function getCoord(
-      windowSize: number,
-      boardSize: number,
-      prevCoord: number,
-      clientPos: number
-    ): (s: number) => number {
-      return s => {
-        const min = windowSize - boardSize * s
-
-        const ratio = s / posRef.current.s
-        const X = zoomIn ? clientPos : windowSize / 2
-
-        return bounded(min, 0)((1 - ratio) * X + ratio * prevCoord)
-      }
+    function setBoardProps(props: Partial<BoardProps>) {
+      posRef.current = { ...posRef.current, ...props }
+      set(props)
     }
   }
+}
+
+function getS(oldS: number, deltaY: number, minScaleW: number, minScaleH: number) {
+  const minScale = Math.min(minScaleW, minScaleH)
+  const scale = oldS + deltaY * -0.03
+  return scale < minScale ? minScale : maxScale < scale ? maxScale : scale
+}
+
+function getCoord(
+  oldS: number,
+  minScale: number,
+  windowSize: number,
+  boardSize: number,
+  prevCoord: number,
+  clientPos: number,
+  newS: number
+): number {
+  return newS < minScale
+    ? (windowSize - boardSize * newS) / 2
+    : (() => {
+        const min = windowSize - boardSize * newS
+        const ratio = newS / oldS
+        return bounded(min, 0)((1 - ratio) * clientPos + ratio * prevCoord)
+      })()
 }
 
 function bounded(min: number, max: number): (x: number) => number {
